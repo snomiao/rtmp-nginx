d9334c444243e1a6c8e176429ed4e421bdf4eeba
snomiao <snomiao@gmail.com>
2025-03-28 07:56:44 +0000

    fix(master): ready
    ready

--- ssl.sh
+++ ssl.sh
@@ -10,4 +10,6 @@ docker run -v ./caddy_data:/data --name rtmpcaddy -p 80:80 -p 443:443 caddy cadd
 cat ./caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$NGINX_SERVERNAME/$NGINX_SERVERNAME.crt | tee ./conf/ssl/fullchain.pem
 cat ./caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$NGINX_SERVERNAME/$NGINX_SERVERNAME.key | tee ./conf/ssl/privkey.pem
 
-# ok
+# ok, you're ready to 
+# next:
+# docker compose up --build
