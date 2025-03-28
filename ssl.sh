6f156b33824af66a6f9c81edb419439623d543a9
snomiao <snomiao@gmail.com>
2025-03-28 07:53:45 +0000

    fix(master): merge servername ver
    merge servername ver

--- ssl.sh
+++ ssl.sh
@@ -4,10 +4,10 @@ source .env
 # run caddy once and wait for certificate logs
 mkdir -p ./conf/ssl/
 docker rm -f rtmpcaddy
-docker run -v ./caddy_data:/data --name rtmpcaddy -p 80:80 -p 443:443 caddy caddy reverse-proxy --from $RTMP_HOSTNAME --to http://localhost
+docker run -v ./caddy_data:/data --name rtmpcaddy -p 80:80 -p 443:443 caddy caddy reverse-proxy --from $NGINX_SERVERNAME --to http://localhost
 
 # copy cert to ssl path
-cat ./caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$RTMP_HOSTNAME/$RTMP_HOSTNAME.crt | tee ./conf/ssl/fullchain.pem
-cat ./caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$RTMP_HOSTNAME/$RTMP_HOSTNAME.key | tee ./conf/ssl/privkey.pem
+cat ./caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$NGINX_SERVERNAME/$NGINX_SERVERNAME.crt | tee ./conf/ssl/fullchain.pem
+cat ./caddy_data/caddy/certificates/acme-v02.api.letsencrypt.org-directory/$NGINX_SERVERNAME/$NGINX_SERVERNAME.key | tee ./conf/ssl/privkey.pem
 
 # ok
